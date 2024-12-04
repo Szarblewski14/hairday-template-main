fdacc7f83b2502090f018ac07751a64ad76efc9e
Leonardo Szarblewski <leonardo27.laurindo@gmail.com>
2024-12-04 14:41:53 -0300

    carregando o html

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path")
+const HtmlWebpackPlugin = require("html-webpack-plugin")
 
 module.exports = {
     target: "web",
@@ -10,12 +11,16 @@ module.exports = {
         path: path.resolve(__dirname, "dist")
     },
 
-    devServer:{
-        static:{
+    devServer: {
+        static: {
             directory: path.join(__dirname, "dist")
         },
-        port:3000,
+        port: 3000,
         open: true,
         liveReload: true,
     },
+
+    plugins: [new HtmlWebpackPlugin({
+        template: path.resolve(__dirname, "index.html"),
+    })],
 }
