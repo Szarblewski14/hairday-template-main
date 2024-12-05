d9621bded900c4434ef9f2fa2a3ad4b045906be6
Leonardo Szarblewski <leonardo27.laurindo@gmail.com>
2024-12-05 14:55:03 -0300

    Renderizando os horários

--- src/modules/form/hours-load.js
+++ src/modules/form/hours-load.js
@@ -1,5 +1,8 @@
 import { openingHours } from "../../utils/opening-hours.js"
+
 import dayjs from "dayjs"
+
+const hours = document.getElementById("hours")
 
 export function hoursLoad({date}) {
     const opening = openingHours.map((hour) => {
@@ -13,5 +16,15 @@ export function hoursLoad({date}) {
             hour,
             available: isHourPast,
         }     
+    })
+
+    //Renderizar os horários
+    opening.forEach(({ hour, available }) =>{
+        const li = document.createElement("li")
+        li.classList.add("hour")
+        li.classList.add(available ? "hour-available" : "hour-unavailable")
+
+        li.textContent = hour
+        hours.append(li)
     })
 }
